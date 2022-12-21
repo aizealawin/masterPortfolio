2f79682e6d72abe25b3412fe491f7a5fb1d04d11
aizealawin <ktmudalige@gmail.com>
2022-12-21 11:01:14 -0500

    typos

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
     "https://docs.google.com/document/d/1bTVsEuUaccmmnmJTBzmsTMPxGWyk5YdaseRwKiqPcl0/edit?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+  portfolio_repository: "https://github.com/aizealawin/masterPortfolio",
   githubProfile: "https://github.com/aizealawin",
 };
 
