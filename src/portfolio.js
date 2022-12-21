431b4b5479a45abbd301cab0a28249d282e63b3b
aizealawin <ktmudalige@gmail.com>
2022-12-21 10:53:09 -0500

    updated resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
+    "https://docs.google.com/document/d/1bTVsEuUaccmmnmJTBzmsTMPxGWyk5YdaseRwKiqPcl0/edit?usp=sharing",
   portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
   githubProfile: "https://github.com/aizealawin",
 };
